6387d7e8f2fb92d3bd431d904d1f77f1754ab1d3
Ashvini Thomas <ashvinithomas@gmail.com>
2020-11-29 11:40:04 -0500

    fixed error

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,6 +8,6 @@ module.exports = function(app) {
     });
 
     app.get("*", function(req, res) {
-        res.sednFile(path.join(__dirname, "../public/index.html"))
+        res.sendFile(path.join(__dirname, "../public/index.html"))
     });
 };
