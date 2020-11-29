39dfa039a4d3b8c00bed6f1b9e33e67f546901ec
Ashvini Thomas <ashvinithomas@gmail.com>
2020-11-29 11:36:01 -0500

    added path to html files

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
 // ================================================================================
 // ROUTER
