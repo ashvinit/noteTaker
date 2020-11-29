bc2a6db2fde517398d6df1fa8ef17636ba7c6b29
Ashvini Thomas <ashvinithomas@gmail.com>
2020-11-29 11:52:34 -0500

    created unique id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,8 @@
 var db = require("../db/db");
+var uniqueString = require('unique-string');
+var fs = require("fs");
+
+ 
 
 module.exports = function(app) {
 
@@ -7,6 +11,7 @@ module.exports = function(app) {
     });
 
     app.post("/api/notes", function(req, res) {
+        req.body.id = uniqueString();
         db.push(req.body);
         return res.json(true);
     });
