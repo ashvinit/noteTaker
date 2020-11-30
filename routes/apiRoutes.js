a1242adaedda9fc9a1d1e0295440a749e7302c36
Ashvini Thomas <ashvinithomas@gmail.com>
2020-11-29 19:28:54 -0500

    created delete method to findIndex of the notes with a particular id and delete that id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,9 @@
 var db = require("../db/db");
 var uniqueString = require('unique-string');
 var fs = require("fs");
+var util = require ('util');
 
- 
+var writeFileAsync = util.promisify(fs.writeFile);
 
 module.exports = function(app) {
 
@@ -10,9 +11,39 @@ module.exports = function(app) {
         res.json(db);
     });
 
-    app.post("/api/notes", function(req, res) {
+    app.post("/api/notes", async function(req, res) {
         req.body.id = uniqueString();
         db.push(req.body);
-        return res.json(true);
+
+        try {
+            await writeFileAsync('./db/db.json', JSON.stringify(db));
+            console.log("updated database");
+            return res.json(true);
+        }
+        catch(err) {
+            console.log(err)
+            return res.json(false);
+        }
+    });
+
+    app.delete("/api/notes/:id", async function(req, res) {
+        console.log(req.url);
+
+        var index = db.findIndex(function(note){
+            return note.id === req.params.id;
+        })
+        if (index !== -1) db.splice(index, 1);
+        console.log('db:', db)
+
+        try {
+            await writeFileAsync('./db/db.json', JSON.stringify(db));
+            console.log("updated database");
+            return res.json(true);
+        }
+        catch(err) {
+            console.log(err)
+            return res.json(false);
+        }
     });
 }
+
